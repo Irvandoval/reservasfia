aabf9c489cc0d8ea9aa23fe22ff4d95e983c6156
Michelle Guillen <jmichwow@gmail.com>
2015-11-01 01:04:58 -0600

    prueba

--- client/app/ciclo/ciclo.controller.js
+++ client/app/ciclo/ciclo.controller.js
@@ -3,7 +3,7 @@
 angular.module('reservasApp')
   .controller('CicloCtrl', function ($scope, $modal) {
    $scope.nuevoCiclo = function(){
-  
+
     var modalInstance = $modal.open({
       animation: $scope.animationsEnabled,
       templateUrl: 'nuevo-ciclo.html',
@@ -11,3 +11,23 @@ angular.module('reservasApp')
     });
    };
   });
+
+  $scope.editarCiclo = function(ciclo){
+   var modalInstance = $modal.open({
+     animation: $scope.animationsEnabled,
+     templateUrl: 'editar-ciclo.html',
+     controller: 'EditarCicloCtrl',
+     size: 'lg',
+     resolve: {
+       aula: function() {
+         return ciclo;
+       }
+     }
+   });
+  }
+  })
+
+  .controller('EditarCicloCtrl',function(ciclo, $scope){
+    $scope.ciclo = ciclo;
+    console.log($scope.ciclo);
+  })

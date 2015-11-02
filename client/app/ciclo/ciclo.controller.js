5daa839c43211ebbb125e4ba55ca1a1b94812bc7
Michelle Guillen <jmichwow@gmail.com>
2015-11-02 00:10:58 -0600

    prueba

--- client/app/ciclo/ciclo.controller.js
+++ client/app/ciclo/ciclo.controller.js
@@ -1,9 +1,26 @@
 'use strict';
 
 angular.module('reservasApp')
-  .controller('CicloCtrl', function ($scope, $modal) {
-   $scope.nuevoCiclo = function(){
+    .controller('CicloCtrl', function ($scope, $resource, ngTableParams, $filter, Ciclo, $modal) {
+     $scope.tableParams = new ngTableParams({
+           page: 1,            // show first page
+           count: 5          // count per page
+       }, {
+           total:0,
+           getData: function ($defer, params) {
+           Ciclo.query().$promise
+           .then(function(ciclos){
+            var orderedRecentActivity = params.filter() ?
+                          $filter('filter')(ciclos, params.filter()) :
+                          ciclos;
+               params.total(orderedRecentActivity.length);
+               $defer.resolve(orderedRecentActivity.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+           })
 
+           }
+       });
+
+   $scope.nuevoCiclo = function(){
     var modalInstance = $modal.open({
       animation: $scope.animationsEnabled,
       templateUrl: 'nuevo-ciclo.html',

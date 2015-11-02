2e40c02ffc6efb85f2bcd953daef23077a38990c
Irvin Sandoval <irvandoval@gmail.com>
2015-11-02 14:36:55 -0600

    something

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/reservas-dev'
+    uri: 'mongodb://admin:reservas@ds031903.mongolab.com:31903/reservas-fia'
   },
 
   seedDB: true

c3cf63c38e964061946540ea0d7142e989e71d9f
Irvin Sandoval <irvandoval@gmail.com>
2016-01-14 00:31:32 -0600

    add uri mongodb in production

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/reservas'
+            'mongodb://admin:reservas@ds031903.mongolab.com:31903/reservas-fia' 
   }
 };
